d39ff9f03b9b8bb86b9c32e622ee97287bb0e180
burak-albayrak <burak_albayrak0@icloud.com>
2024-08-31 02:26:32 +0300

    Update some unit symbols and explanation syntax order

--- UnitConverter/Model/UnitModel.swift
+++ UnitConverter/Model/UnitModel.swift
@@ -21,42 +21,42 @@ enum Category: String, CaseIterable {
     var icon: String {
         switch self {
         case .mass:
-            return "case"
+            return "scalemass"
         case .length:
             return "ruler"
         case .area:
-            return "map"
+            return "skew"
         case .volume:
-            return "cube"
+            return "pyramid"
         case .pressure:
-            return "hammer"
+            return "barometer"
         case .temperature:
             return "thermometer"
         case .angle:
-            return "pyramid"
+            return "angle"
         case .speed:
-            return "speedometer"
+            return "gauge.with.dots.needle.67percent"
         case .duration:
-            return "clock.arrow.2.circlepath"
+            return "stopwatch"
         }
     }
     
     var info: String {
         switch self {
-        case .angle:
-            return "Angle is a quantity of rotation. The SI unit for angle is the radian (rad), which is dimensionless and defined to be the angle subtended by an arc that is equal in length to the radius of a circle. Angle is also commonly expressed in terms of degrees (°) and revolutions (rev)."
-        case .length:
-            return "Length is the dimensional extent of matter. The SI unit for length is the meter (m), which is defined in terms of the distance traveled by light in a vacuum."
         case .mass:
             return "Mass is a fundamental property of matter that causes it to resist being accelerated by a force. The SI unit for mass is the kilogram (kg), which is defined in terms of the mass of the international prototype kilogram."
+        case .length:
+            return "Length is the dimensional extent of matter. The SI unit for length is the meter (m), which is defined in terms of the distance traveled by light in a vacuum."
         case .area:
             return "Area is a quantity of extent in two dimensions. Area can be expressed by SI derived units in terms of square meters (m2). Area is also commonly measured in square feet (ft2) and acres (ac)."
         case .volume:
             return "Volume is a quantity of the extent of matter in three dimensions. The SI accepted unit of volume is the liter (L), which is derived as one cubic decimeter (1 dm3). Volume is also commonly expressed in terms of cubic meters (m3), gallons (gal), and cups (cup)."
-        case .temperature:
-            return "Temperature is a comparative measure of thermal energy. The SI unit for temperature is the kelvin (K), which is defined in terms of the triple point of water. Temperature is also commonly measured by degrees of various scales, including Celsius (°C) and Fahrenheit (°F)."
         case .pressure:
             return "Pressure is the normal force over a surface. The SI unit for pressure is the pascal (Pa), which is derived as one newton of force over one square meter (1Pa = 1N / 1m2)."
+        case .temperature:
+            return "Temperature is a comparative measure of thermal energy. The SI unit for temperature is the kelvin (K), which is defined in terms of the triple point of water. Temperature is also commonly measured by degrees of various scales, including Celsius (°C) and Fahrenheit (°F)."
+        case .angle:
+            return "Angle is a quantity of rotation. The SI unit for angle is the radian (rad), which is dimensionless and defined to be the angle subtended by an arc that is equal in length to the radius of a circle. Angle is also commonly expressed in terms of degrees (°) and revolutions (rev)."
         case .speed:
             return "Speed is the magnitude of velocity, or the rate of change of position. Speed can be expressed by SI derived units in terms of meters per second (m/s), and is also commonly expressed in terms of kilometers per hour (km/h) and miles per hour (mph)."
         case .duration:
