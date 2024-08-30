d322e24b1c8c227ad88fc444a3dbb1d98aed5ac8
burak-albayrak <burak_albayrak0@icloud.com>
2024-08-31 02:43:52 +0300

    Update ViewModel Syntax

--- UnitConverter/ViewModel/UnitConverterViewModel.swift
+++ UnitConverter/ViewModel/UnitConverterViewModel.swift
@@ -14,11 +14,11 @@ final class DetailViewModel: ObservableObject {
     @Published var secondUnitInputValue = ""
     @Published var showingInfo = false
     
+    var category: Category
+    
     var availableUnits: [Dimension] {
         return unitsForCategory(category: category)
     }
-    
-    var category: Category
     
     init(category: Category) {
         self.category = category
@@ -35,62 +35,147 @@ final class DetailViewModel: ObservableObject {
         switch category {
         case .area:
             return [
-                UnitArea.squareMegameters, UnitArea.squareKilometers, UnitArea.squareMeters, UnitArea.squareCentimeters,
-                UnitArea.squareMillimeters, UnitArea.squareNanometers, UnitArea.squareInches, UnitArea.squareFeet,
-                UnitArea.squareYards, UnitArea.squareMiles, UnitArea.acres, UnitArea.ares, UnitArea.hectares
+                UnitArea.squareMegameters, 
+                UnitArea.squareKilometers,
+                UnitArea.squareMeters,
+                UnitArea.squareCentimeters,
+                UnitArea.squareMillimeters, 
+                UnitArea.squareNanometers,
+                UnitArea.squareInches,
+                UnitArea.squareFeet,
+                UnitArea.squareYards,
+                UnitArea.squareMiles,
+                UnitArea.acres,
+                UnitArea.ares,
+                UnitArea.hectares
             ]
             
         case .length:
             return [
-                UnitLength.megameters, UnitLength.kilometers, UnitLength.hectometers, UnitLength.decameters, UnitLength.meters,
-                UnitLength.decimeters, UnitLength.centimeters, UnitLength.millimeters, UnitLength.micrometers, UnitLength.nanometers,
-                UnitLength.picometers, UnitLength.inches, UnitLength.feet, UnitLength.yards, UnitLength.miles,
-                UnitLength.scandinavianMiles, UnitLength.lightyears, UnitLength.nauticalMiles, UnitLength.fathoms,
-                UnitLength.astronomicalUnits, UnitLength.parsecs
+                UnitLength.megameters,
+                UnitLength.kilometers,
+                UnitLength.hectometers,
+                UnitLength.decameters,
+                UnitLength.meters,
+                UnitLength.decimeters, 
+                UnitLength.centimeters,
+                UnitLength.millimeters,
+                UnitLength.micrometers,
+                UnitLength.nanometers,
+                UnitLength.picometers, 
+                UnitLength.inches,
+                UnitLength.feet,
+                UnitLength.yards,
+                UnitLength.miles,
+                UnitLength.scandinavianMiles, 
+                UnitLength.lightyears,
+                UnitLength.nauticalMiles,
+                UnitLength.fathoms,
+                UnitLength.astronomicalUnits,
+                UnitLength.parsecs
             ]
             
         case .pressure:
             return [
-                UnitPressure.newtonsPerMetersSquared, UnitPressure.gigapascals, UnitPressure.megapascals,
-                UnitPressure.kilopascals, UnitPressure.hectopascals, UnitPressure.inchesOfMercury, UnitPressure.bars,
-                UnitPressure.millibars, UnitPressure.millimetersOfMercury, UnitPressure.poundsForcePerSquareInch
+                UnitPressure.newtonsPerMetersSquared, 
+                UnitPressure.gigapascals,
+                UnitPressure.megapascals,
+                UnitPressure.kilopascals, 
+                UnitPressure.hectopascals,
+                UnitPressure.inchesOfMercury,
+                UnitPressure.bars,
+                UnitPressure.millibars, 
+                UnitPressure.millimetersOfMercury,
+                UnitPressure.poundsForcePerSquareInch
             ]
             
         case .temperature:
-            return [UnitTemperature.celsius, UnitTemperature.fahrenheit, UnitTemperature.kelvin]
+            return [UnitTemperature.celsius, 
+                    UnitTemperature.fahrenheit,
+                    UnitTemperature.kelvin]
             
         case .volume:
             return [
-                UnitVolume.megaliters, UnitVolume.kiloliters, UnitVolume.liters, UnitVolume.deciliters, UnitVolume.centiliters,
-                UnitVolume.milliliters, UnitVolume.cubicKilometers, UnitVolume.cubicMeters, UnitVolume.cubicDecimeters,
-                UnitVolume.cubicCentimeters, UnitVolume.cubicMillimeters, UnitVolume.cubicInches, UnitVolume.cubicFeet,
-                UnitVolume.cubicYards, UnitVolume.cubicMiles, UnitVolume.acreFeet, UnitVolume.bushels, UnitVolume.teaspoons,
-                UnitVolume.tablespoons, UnitVolume.fluidOunces, UnitVolume.cups, UnitVolume.pints, UnitVolume.quarts,
-                UnitVolume.gallons, UnitVolume.imperialTeaspoons, UnitVolume.imperialTablespoons, UnitVolume.imperialFluidOunces,
-                UnitVolume.imperialPints, UnitVolume.imperialQuarts, UnitVolume.imperialGallons, UnitVolume.metricCups
+                UnitVolume.megaliters, 
+                UnitVolume.kiloliters,
+                UnitVolume.liters,
+                UnitVolume.deciliters,
+                UnitVolume.centiliters,
+                UnitVolume.milliliters, 
+                UnitVolume.cubicKilometers,
+                UnitVolume.cubicMeters,
+                UnitVolume.cubicDecimeters,
+                UnitVolume.cubicCentimeters, 
+                UnitVolume.cubicMillimeters,
+                UnitVolume.cubicInches,
+                UnitVolume.cubicFeet,
+                UnitVolume.cubicYards, 
+                UnitVolume.cubicMiles,
+                UnitVolume.acreFeet,
+                UnitVolume.bushels,
+                UnitVolume.teaspoons,
+                UnitVolume.tablespoons, 
+                UnitVolume.fluidOunces,
+                UnitVolume.cups,
+                UnitVolume.pints,
+                UnitVolume.quarts,
+                UnitVolume.gallons, 
+                UnitVolume.imperialTeaspoons,
+                UnitVolume.imperialTablespoons,
+                UnitVolume.imperialFluidOunces,
+                UnitVolume.imperialPints, 
+                UnitVolume.imperialQuarts,
+                UnitVolume.imperialGallons,
+                UnitVolume.metricCups
             ]
             
         case .mass:
             return [
-                UnitMass.kilograms, UnitMass.grams, UnitMass.decigrams, UnitMass.centigrams, UnitMass.milligrams,
-                UnitMass.micrograms, UnitMass.nanograms, UnitMass.picograms, UnitMass.ounces, UnitMass.pounds, UnitMass.stones,
-                UnitMass.metricTons, UnitMass.shortTons, UnitMass.carats, UnitMass.ouncesTroy, UnitMass.slugs
+                UnitMass.kilograms, 
+                UnitMass.grams,
+                UnitMass.decigrams,
+                UnitMass.centigrams,
+                UnitMass.milligrams,
+                UnitMass.micrograms, 
+                UnitMass.nanograms,
+                UnitMass.picograms,
+                UnitMass.ounces,
+                UnitMass.pounds,
+                UnitMass.stones,
+                UnitMass.metricTons, 
+                UnitMass.shortTons,
+                UnitMass.carats,
+                UnitMass.ouncesTroy,
+                UnitMass.slugs
             ]
             
         case .angle:
             return [
-                UnitAngle.degrees, UnitAngle.arcMinutes, UnitAngle.arcSeconds, UnitAngle.radians, UnitAngle.gradians, UnitAngle.revolutions
+                UnitAngle.degrees, 
+                UnitAngle.arcMinutes,
+                UnitAngle.arcSeconds,
+                UnitAngle.radians,
+                UnitAngle.gradians,
+                UnitAngle.revolutions
             ]
             
         case .speed:
             return [
-                UnitSpeed.metersPerSecond, UnitSpeed.kilometersPerHour, UnitSpeed.milesPerHour, UnitSpeed.knots
+                UnitSpeed.metersPerSecond, 
+                UnitSpeed.kilometersPerHour,
+                UnitSpeed.milesPerHour,
+                UnitSpeed.knots
             ]
             
         case .duration:
             return [
-                UnitDuration.hours, UnitDuration.minutes, UnitDuration.seconds, UnitDuration.milliseconds, UnitDuration.microseconds,
-                UnitDuration.nanoseconds, UnitDuration.picoseconds
+                UnitDuration.hours, 
+                UnitDuration.minutes,
+                UnitDuration.seconds,
+                UnitDuration.milliseconds,
+                UnitDuration.microseconds,
+                UnitDuration.nanoseconds, 
+                UnitDuration.picoseconds
             ]
         }
     }
