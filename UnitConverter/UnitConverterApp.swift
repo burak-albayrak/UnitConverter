f5b711f6e5d0d5c3c356d900a690220c33704101
burak-albayrak <burak_albayrak0@icloud.com>
2024-08-31 01:58:11 +0300

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Burak Albayrak on 31.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
