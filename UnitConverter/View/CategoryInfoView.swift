9ad2fcecb1885b8ede0e0ab2745f10ca72f64c7e
burak-albayrak <burak_albayrak0@icloud.com>
2024-08-31 03:31:04 +0300

    Creating CategoryInfoView and fixing something

--- UnitConverter/View/CategoryInfoView.swift
+++ UnitConverter/View/CategoryInfoView.swift
@@ -8,11 +8,41 @@
 import SwiftUI
 
 struct CategoryInfoView: View {
+    @Environment(\.presentationMode) var presentationMode
+    var category: Category
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Image(systemName: category.icon)
+                    .foregroundColor(.blue)
+                Text(category.rawValue)
+                    .font(.title2)
+                    .bold()
+            }
+            .padding(.top, 70)
+
+            Text(category.info)
+                .padding(20)
+            
+            Spacer()
+            
+            Button(action: {
+                presentationMode.wrappedValue.dismiss()
+            }, label: {
+                Text("OK")
+                    .font(.headline)
+                    .bold()
+                    .foregroundColor(.white)
+                    .frame(width: 280, height: 44)
+                    .background(Color.blue)
+                    .cornerRadius(12)
+            })
+                .padding(.bottom, 60)
+        }
     }
 }
 
 #Preview {
-    CategoryInfoView()
+    CategoryInfoView(category: .mass)
 }
